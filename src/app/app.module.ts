b158580a3ac2691ada27eac835f69229843708c5
aertonma9 <aerton.oliveira@ma9.com.br>
2017-12-27 20:12:14 -0300

    Modulo de Shared module funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,13 +18,11 @@ import { MenuItemComponent } from './restaurant-detalhi/menu-item/menu-item.comp
 import { ReviewsComponent } from './restaurant-detalhi/reviews/reviews.component';
 import {ShoppingCartService} from './restaurant-detalhi/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component';
 import {OrderService} from './order/order.service';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
+import { SharedModule } from 'app/shared/shared.module';
 
 
 @NgModule({
@@ -40,18 +38,14 @@ import { RatingComponent } from './shared/rating/rating.component';
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent,
-    RadioComponent,
     OrderItemsComponent,
     DeliveryCostsComponent,
     OrderSummaryComponent,
-    RatingComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule,
-    ReactiveFormsModule,
+    SharedModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [RestaurantsService,ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'} ],
