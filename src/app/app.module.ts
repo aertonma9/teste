cf15d3e1bf52f021ee9f035e4a8c64de99e3f926
aertonma9 <aerton.oliveira@ma9.com.br>
2017-12-28 20:08:33 -0300

    M

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,11 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import {RestaurantsService} from './restaurants/restaurants.service';
 import { RestaurantDetalhiComponent } from './restaurant-detalhi/restaurant-detalhi.component';
 import { MenuComponent } from './restaurant-detalhi/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detalhi/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detalhi/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detalhi/reviews/reviews.component';
-import {ShoppingCartService} from './restaurant-detalhi/shopping-cart/shopping-cart.service';
-import {OrderService} from './order/order.service';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from 'app/shared/shared.module';
 
@@ -42,7 +39,7 @@ import { SharedModule } from 'app/shared/shared.module';
     SharedModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService,ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'} ],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
